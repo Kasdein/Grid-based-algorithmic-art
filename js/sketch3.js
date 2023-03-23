66e14acc1c02550dfd2a0861a1676b2a12a7de1c
Kasdein <kasdein@outlook.fr>
2023-03-23 12:11:16 +0000

    made it prettier

--- js/sketch3.js
+++ js/sketch3.js
@@ -7,7 +7,7 @@ let max = 50
 let positiveList = []
 let negativeList = []
 
-String.prototype.convertToRgb = function() {
+String.prototype.convertToRgb = function () {
     let fromHexToRgb = this.match(/.{1,2}/g)
     let toRgb = [
         parseInt(fromHexToRgb[0], 16),
@@ -19,46 +19,51 @@ String.prototype.convertToRgb = function() {
 
 function Rectangle(x, y) {
     rgbColours = [hexColours[0].convertToRgb(), hexColours[1].convertToRgb(), hexColours[2].convertToRgb(), hexColours[3].convertToRgb(), hexColours[4].convertToRgb()]
-    this.show = function() {
+    this.show = function () {
 
         noStroke()
-        for(i = 0; i <= 4; i++){
-        fill(random(rgbColours))
-        quad(x + positiveList[0 + i], y, x, y + positiveList[0 + i], x + negativeList[0 + i], y, x, y + negativeList[0 + i])
+        for (i = 0; i <= 4; i++) {
+            fill(random(rgbColours))
+            quad(x + positiveList[1 + i], y, x, y + positiveList[1 + i], x + negativeList[1 + i], y, x, y + negativeList[1 + i])
         }
+
+        // for (i = 0; i <= 4; i++) {
+        //     fill(random(rgbColours))
+        //     quad(x + positiveList[0 + i], y, x, y + positiveList[0 + i], x + negativeList[0 + i], y, x, y + negativeList[0 + i])
+        // }
     }
 }
 
-function setup(){
-    for(i = 0; i <= 4; i++){
+function setup() {
+    for (i = 0; i <= 4; i++) {
         positiveList.push(positive - (inc * i))
         negativeList.push(negative + (inc * i))
     }
     const cellSize = 25
-    gridSize = 750 
+    gridSize = 750
     noLoop()
-    frameRate(1)
+    // frameRate(1)
 
     createCanvas(gridSize + cellSize, gridSize + cellSize)
     strokeWeight(5)
     stroke(0)
 
-    for(let a = 0; a < gridSize - inc; a += max){
-        for(let b = 0; b < gridSize - inc; b += max){
+    for (let a = 0; a < gridSize - inc; a += max) {
+        for (let b = 0; b < gridSize - inc; b += max) {
             rectangles.push(new Rectangle(a, b))
         }
     }
 }
 
-function draw(){
+function draw() {
     background(0)
     translate(positive, positive)
-    for (let b = 0; b < rectangles.length; b++){
+    for (let b = 0; b < rectangles.length; b++) {
         rectangles[b].show()
     }
     push()
     translate(positive, positive)
-    for (let b = 0; b < rectangles.length; b++){
+    for (let b = 0; b < rectangles.length; b++) {
         rectangles[b].show()
     }
     pop()
